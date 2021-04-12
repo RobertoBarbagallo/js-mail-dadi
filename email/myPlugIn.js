5b333e2b3004416de9386bab6710a3601e07b1c1
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-12 18:08:32 +0200

    utilizza for per il match cdei valori

--- email/myPlugIn.js
+++ email/myPlugIn.js
@@ -1,31 +1,35 @@
 var emailValide = ["fabio@boolean.it", "roberto@boolean.it", "giacomo@boolean.it", "diana@boolean.it", "sergio@boolean.it", "antonio@boolean.it", "gabriella@boolean.it", "michela@boolean.it"];
+var nuoveIscrizioni = [];
+var emailInserita = prompt("Inserisci la tua email per accedere al sito");
+var i = 0;
+var trovato = false;
 
-var emailInserita = prompt ("Inserisci la tua email per accedere al sito");
+for (var i = 0; i < emailValide.length; i++) {
+    if (emailInserita == emailValide[i]) {
+        trovato = true
+    } else {
+        nuoveIscrizioni.push(emailInserita);
+    }
+};
+console.log("nuova iscrizione " + nuoveIscrizioni)
+console.log(trovato)
+if (trovato) {
+    document.getElementById("target").innerHTML = emailInserita + " è una email valida, a breve verrai reindirizzato";
+} else {
 
-var autenticazione = emailValide.includes(emailInserita);
+    if (nuoveIscrizioni.length > 1) {
+        var richiestaNuovaIscrizione = prompt(emailInserita + " non è una email valida, prova ad iscriverti al volo inserendo nuovamente la tua mail");
+        if (emailInserita === richiestaNuovaIscrizione) {
+            document.getElementById("target").innerHTML = emailInserita + " è ora una email valida, a breve verrai reindirizzato - Grazie per esserti iscritto";
+        } else {
+            document.getElementById("target").innerHTML = emailInserita + ", la prima mail che hai inserito, è diversa da " + richiestaNuovaIscrizione + " prova a ricaricare la pagina";
+        }
 
-
-if (autenticazione === true) {
-    document.getElementById("target").innerHTML= emailInserita + " è una email valida, a breve verrai reindirizzato";
-}else {
-        var richiestaNuovaIscrizione = prompt (emailInserita + " non è una email valida, prova ad iscriverti al volo inserendo nuovamente la tua mail");
-
-         if (richiestaNuovaIscrizione === emailInserita){
-
-            emailValide.push(richiestaNuovaIscrizione);
-
-            var nuovaRegistrazione = emailValide.includes(richiestaNuovaIscrizione);
-
-                 if (nuovaRegistrazione === true){
-                    document.getElementById("target").innerHTML= emailInserita + " è ora una email valida, a breve verrai reindirizzato - Grazie per esserti iscritto";}
-                       
-         }else{      
-            document.getElementById("target").innerHTML= emailInserita + ", la prima mail che hai inserito, è diversa da " + richiestaNuovaIscrizione + " prova a ricaricare la pagina";}
-
-    }   
+    } else {
+        console.log("ERROR");
+    };
+};
 
 console.log(emailInserita);
 console.log(richiestaNuovaIscrizione);
-console.log(nuovaRegistrazione);
 console.log(emailValide);
-    
